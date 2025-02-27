2f33f34344a7c083e43d164ab6b259cba12536a5
vinycius <vinyciusalves155@gmail.com>
2025-02-27 13:07:09 -0300

    adicao da classe de votacao

---
+++ src/votacao/Votacao.java
@@ -0,0 +1,69 @@
+package votacao;
+
+import eleicao.Eleicao;
+
+public class Votacao {
+
+    private int votoBranco;
+    private int votoNulo;
+    private int votosValidos;
+    private int votosTotais;
+    private int limiteVotos;
+    private Eleicao eleicao;
+
+    public Votacao(Eleicao eleicao, int limiteVotos) {
+        this.votoBranco = 0;
+        this.votoNulo = 0;
+        this.votosValidos = 0;
+        this.votosTotais = 0;
+        this.limiteVotos = limiteVotos;
+        this.eleicao = eleicao;
+    }
+
+    public void addBranco() {
+        this.votoBranco++;
+    }
+
+    public void addNulo() {
+        this.votoNulo++;
+    }
+
+    public void addValido() {
+        this.votosValidos++;
+    }
+
+    public void contabilizarVoto() {
+        this.votosTotais++;
+        if (this.votosTotais >= this.limiteVotos) {
+            eleicao.finalizar();
+        }
+    }
+
+    public int getBranco() {
+        return votoBranco;
+    }
+
+    public int getNulo() {
+        return votoNulo;
+    }
+
+    public int getVotosValidos() {
+        return votosValidos;
+    }
+
+    public int getVotosTotais() {
+        return votosTotais;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("VOTOS BRANCOS: ").append(votoBranco).append("\n");
+        sb.append("VOTOS NULOS: ").append(votoNulo).append("\n");
+        sb.append("VOTOS VALIDOS: ").append(votosValidos).append("\n");
+        sb.append("VOTOS TOTAIS: ").append(votosTotais).append("\n");
+
+        return sb.toString();
+    }
+
+}
