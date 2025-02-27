9e72e83d423dcae736711d21f17e7860775ca509
vinycius <vinyciusalves155@gmail.com>
2025-02-27 00:20:58 -0300

    adicao da clase eleicao refatorada

---
+++ src/eleicao/Eleicao.java
@@ -0,0 +1,34 @@
+package eleicao;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Eleicao {
+    private boolean ativa;
+    private LocalTime horarioTermino;
+
+    public Eleicao() {
+        this.ativa = false;
+    }
+
+    // INICIA A ELEICAO PARA RECEBER VOTOS
+    public void iniciar() {
+        this.ativa = true;
+    }
+
+    // FINALIZA A VOTACAO, NAO PERMITIDO MAIS VOTOS
+    public void finalizar() {
+        this.ativa = false;
+    }
+
+    // DEFINE UM HORARIO PARA O TERMINO DA VOTACAO
+    public void definirHorarioTermino(String horario) {
+        horarioTermino = LocalTime.parse(horario, DateTimeFormatter.ofPattern("HH:mm"));
+    }
+
+    // VERIFICA SE A VOTACAO ESTA ATIVA OU NAO
+    public boolean isAtiva() {
+        return ativa && (horarioTermino == null || LocalTime.now().isBefore(horarioTermino));
+    }
+
+}
